861550f77fc2c9c35180091cf5c10ae75780b2ef
LenLenTM <lena_gross@gmx.at>
2023-01-05 18:05:12 +0100

    custom cat enabled

--- Assets/Code/MainMenu/NewGame/CatGenerator/ChooseCustomCat.cs
+++ Assets/Code/MainMenu/NewGame/CatGenerator/ChooseCustomCat.cs
@@ -1,12 +1,29 @@
+using System.Collections.Generic;
+using System.Text;
+using Code;
 using UnityEngine;
 using TMPro;
+using Random = System.Random;
 
 public class ChooseCustomCat : MonoBehaviour
 {
     public TMP_InputField catNameInput;
+    public List<Sprite> catSpriteList;
 
     private void OnMouseDown()
     {
-        Debug.Log(catNameInput.text);
+        string name = catNameInput.text;
+
+        byte[] by = Encoding.ASCII.GetBytes(catNameInput.text);
+        int catPictureNumber = 0;
+
+        foreach (byte x in by)
+        {
+            catPictureNumber += x;
+        }
+        Random random = new Random(1312);
+        catPictureNumber = (catPictureNumber + random.Next()) % 14;
+
+        GameGenerator.createNewGame(name, catSpriteList[catPictureNumber].name);
     }
 }
